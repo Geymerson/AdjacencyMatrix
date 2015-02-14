177c19a3f2df38387a2997a82329a81603d0a104
Geymerson <geymerson.r@gmail.com>
2015-02-14 12:45:07 -0300

    Some variables names changes

--- DataTreatment.cpp
+++ DataTreatment.cpp
@@ -5,7 +5,7 @@ DataTreatment::DataTreatment() {
     m_myFileName = "";
     m_myProfileName = "";
     m_numberOfFriends = 0;
-}
+}//End class' empty constructor
 
 //Class constructor
 DataTreatment::DataTreatment(QString myFileName,
@@ -15,11 +15,11 @@ DataTreatment::DataTreatment(QString myFileName,
     m_myFileName = myFileName;
     m_myProfileName = myProfileName;
     m_numberOfFriends = numberOfFriends;
-}
+}//End class' non-empty-constructor
 
 DataTreatment::~DataTreatment() {
 
-}
+}//End class' destructor
 
 void DataTreatment::readData() {
     //Reading the entry file
@@ -31,37 +31,41 @@ void DataTreatment::readData() {
         qDebug() << "File Not Opened";
     }
 
-    //A better name is needed to this variables.
-    QString s, t, u;
+    QString line, personName1, personName2;
 
     while(!dataEntry.atEnd()) {
-        s = dataEntry.readLine(1024);
-        t = s.section('"', 1, 1);
-        u = s.section('"', 3, 3);
-        t != m_myProfileName ? m_friendList.append(t):
-                               m_friendList.append(u);
-    }
-}
+        line = dataEntry.readLine(1024);
+        //Extract the first name in the string
+        personName1 = line.section('"', 1, 1);
+
+        //Extract the second name in the string
+        personName2 = line.section('"', 3, 3);
+
+        //Insert the friend's name in the friend
+        //list
+        personName1 != m_myProfileName ?
+                    m_friendList.append(personName1):
+                    m_friendList.append(personName2);
+    }//End loop while
+}//End method readData
 
 //Method to generate the matrix
 void DataTreatment::createMatrix(){
-    int aux = 1;
+    int currentProfile = 1;
 
-    while(aux != m_friendList.length()) {
+    while(currentProfile != m_friendList.length()) {
         //Creating the matrix
         int temp[m_friendList.length()][3];
 
         //Fill rows 0 and 1
         for(int i = 1; i <= m_friendList.length(); i++) {
-            temp[i - 1][0] = aux;
+            temp[i - 1][0] = currentProfile;
             temp[i - 1][1] = i;
+        }//End loop for
 
-            //Every user is related to himself
-            if(i == aux) {
-                temp[i][aux] = 1;
-            }
-        }
-        aux++;
-    }
-}
+        //Every user is related to himself
+        temp[currentProfile][2] = 1;
+        currentProfile++;
+    }//End loop while
+}//End method createMatrix
 
--- main.cpp
+++ main.cpp
@@ -4,9 +4,6 @@
 int main(int argc, char *argv[])
 {
     //QCoreApplication a(argc, argv);
-    DataTreatment test("result_give_me_my _data.txt",
-                       "Marcel Augusto", 3);
-    test.readData();
-
+    return 0;
     //return a.exec();
 }
