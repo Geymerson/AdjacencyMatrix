328be1742923e7e4a3d5976189eacdfa0e9aaf8c
Geymerson <geymerson.r@gmail.com>
2015-02-15 11:58:19 -0300

    readData fixes

--- DataTreatment.cpp
+++ DataTreatment.cpp
@@ -27,16 +27,19 @@ void DataTreatment::readData() {
     QFile dataEntry(m_myFileName);
     dataEntry.open(QIODevice::ReadOnly);
 
-    //Checking if the filed has been openned
+    //Checking if the file has been opened
     if(!dataEntry.isOpen()) {
-        qDebug() << "File Not Opened";
+        qDebug().quote() << "File Not Opened";
     }
+
+    m_friendsRelationListFile = new QFile("FriendsRelations.txt");
+    m_friendsRelationListFile->open(QIODevice::WriteOnly);
 
     QString line, personName1, personName2;
 
-    int lineCount = -1;
+    int lineCount = 0;
 
-    while(!dataEntry.atEnd()) {
+    while(lineCount != m_numberOfFriends) {
         line = dataEntry.readLine(1024);
 
         //Extract the first name in the string
@@ -45,6 +48,9 @@ void DataTreatment::readData() {
         //Extract the second name in the string
         personName2 = line.section('"', 3, 3);
 
+//        qDebug().noquote() << personName1 << ", "
+//                           << personName2;
+
         //Insert the friend's name in the friend
         //list
         personName1 != m_myProfileName ?
@@ -52,24 +58,24 @@ void DataTreatment::readData() {
                     m_friendList.append(personName2);
 
         lineCount++;
-
         if(lineCount == m_numberOfFriends) {
-            m_friendsRelationListFile = new QFile("FriendsRelation.txt");
-            m_friendsRelationListFile->open(QIODevice::WriteOnly);
-            QDataStream out(m_friendsRelationListFile);
-
-            while (!dataEntry.atEnd()) {
-                QString tempLine = dataEntry.read(8000);
-                out << tempLine;
+            while(!dataEntry.atEnd()) {
+                QByteArray getData = dataEntry.read(8000);
+                m_friendsRelationListFile->write(getData);
             }
-        }
-        m_friendsRelationListFile->close();
+        }//End condition if
     }//End loop while
+    m_friendsRelationListFile->close();
 }//End method readData
 
 //Method to generate the matrix
 void DataTreatment::createMatrix(){
+    //Indicator of matrix that need to me made
     bool matrixToBeMade[1000] = {true};
+
+    //Matrix Representation that will
+    //be written to a file
+    QByteArray matrixRepresentation;
     m_friendsRelationListFile->open(QIODevice::ReadOnly);
 
     while(!m_friendsRelationListFile->atEnd()) {
@@ -88,31 +94,55 @@ void DataTreatment::createMatrix(){
         //Get the index of the current person
         int currentProfile = m_friendList.indexOf(personName1) + 1;
 
-        //Fill column 0 and 1
-        for(int i = 1; i <= m_friendList.length(); i++) {
+        //Fill matrix
+        for(int i = 1; i <= m_numberOfFriends; i++) {
             tempMatrix[i - 1][0] = currentProfile;
             tempMatrix[i - 1][1] = i;
             tempMatrix[i - 1][2] = 0;
         }//End loop for
 
-        //Every user is related to himself
+        //Every person is related to itself
         tempMatrix[currentProfile - 1][2] = 1;
 
-        //Person1 is friend of person2
+        //Person1 is related with person2
         tempMatrix[m_friendList.indexOf(personName2)][2] = 1;
 
-        //The current person matrix
+        //The current person's matrix
         //Does not require to be
         //Created
         matrixToBeMade[m_friendList.indexOf(personName1)] = false;
 
-        //Output file
+        //Matrix's output  file
         QFile currentUserMatrix("Matrix " + currentProfile);
         currentUserMatrix.open(QIODevice::WriteOnly);
 
         //Writing the adjacency matrix
         //to a file
-        QDataStream outputMatrix(&currentUserMatrix);
-        outputMatrix << tempMatrix;
+        for(int i = 0; i < m_numberOfFriends; i++) {
+            for(int j = 0; j < 3; j++) {
+                matrixRepresentation.append(tempMatrix[i][j]);
+                if(j == 2) {
+                    matrixRepresentation.append('\n');
+                }
+                else {
+                    matrixRepresentation.append(' ');
+                }
+            }
+        }
+        currentUserMatrix.write(matrixRepresentation);
+        matrixRepresentation.clear();
+        currentUserMatrix.close();
     }//End loop while
+    
+    for(int i = 0; i < 1000; i++) {
+        if(matrixToBeMade[i]) {
+            int tempMatrix[m_numberOfFriends][3];
+            for(int j = 1; j <= m_numberOfFriends; i++) {
+                tempMatrix[j - 1][0] = i + 1;
+                tempMatrix[j - 1][1] = j;
+                tempMatrix[j - 1][2] = 0;
+            }//End loop for
+        }
+    }
+    m_friendsRelationListFile->close();
 }//End method createMatrix
--- main.cpp
+++ main.cpp
@@ -4,6 +4,9 @@
 int main(int argc, char *argv[])
 {
     //QCoreApplication a(argc, argv);
+    DataTreatment sample("result_give_me_my _data.txt", "Marcel Augusto", 3);
+    //sample.readData();
+    //sample.createMatrix();
     return 0;
     //return a.exec();
 }
